2ec0607bbaccac087f4e6dd26bbeb37553405ef8
Skivap <aurickd@gmail.com>
2024-11-09 02:14:12 +0800

    can interact ITWEMMMM

--- Assets/Script/KitchenTable.cs
+++ Assets/Script/KitchenTable.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class KitchenTable : MonoBehaviour
 {
+    [SerializeField] private bool infiniteItem = true;
+    [SerializeField] private GameObject foodObject;
+    
     private MeshRenderer tableRenderer;
     private float originalMetallic;
 
@@ -26,5 +29,20 @@ public class KitchenTable : MonoBehaviour
         {
             tableRenderer.material.SetFloat("_Metallic", highlighted ? 0.8f : originalMetallic); // Adjust metallic value
         }
+    }
+
+    // Put Items
+    public void PutItem(GameObject gameObject){
+        foodObject = gameObject;
+    }
+    
+    // To Take Item From the Desk or Supplies
+    // TODO: If can put multiple things
+    public GameObject TakeItem(){
+        GameObject returnItem = foodObject;
+        if (!infiniteItem) { 
+            foodObject = null;
+        }
+        return returnItem;
     }
 }
--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -6,11 +6,19 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 2.5f;
     [SerializeField] private float raycastRange = 1f;
+    [SerializeField] private float holdDistance = 1.0f;
+    
     private KitchenTable lastHitTable;
+    private GameObject holdItem;
+
+    /*
+        MOVE        : WASD
+        INTERACT    : F
+    */
 
     void Start()
     {
-        
+        holdItem = null;
     }
 
     void Update()
@@ -28,17 +36,45 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) moveVector.x -= 1.0f;
         if (Input.GetKey(KeyCode.D)) moveVector.x += 1.0f;
 
-        // Normalize moveVector if there's any input, then move the player
         if (moveVector != Vector3.zero)
         {
             transform.position += moveVector.normalized * movementSpeed * Time.deltaTime;
-            transform.rotation = Quaternion.LookRotation(moveVector); // Rotate to face movement direction
+            transform.rotation = Quaternion.LookRotation(moveVector);
         }
     }
 
+    private void ObjectInteract(KitchenTable table)
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (holdItem)
+            {
+                table.PutItem(holdItem);
+                holdItem.transform.SetParent(null); // Detach the item when putting it back
+                holdItem = null;
+            }
+            else
+            {
+                GameObject item = table.TakeItem();
+                if (item != null)
+                {
+                    holdItem = Instantiate(item); // Instantiate a copy of the prefab
+                    holdItem.transform.SetParent(transform); // Set the player as the parent
+                    holdItem.transform.localPosition = new Vector3(0, 1, 0.5f); // Position in front of the player
+                }
+            }
+        }
+    }
+
+
+    /*
+        #==================================================#
+        #========= You can ignore below functions =========#
+        #==================================================#
+    */
     private void RayCastObject()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
+        Ray ray = new Ray(transform.position, transform.forward + Vector3.down * 0.5f);
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, raycastRange))
@@ -47,12 +83,16 @@ public class PlayerController : MonoBehaviour
 
             if (table != null)
             {
+                /* Highlighted */
                 table.SetHighlighted(true);
                 if (lastHitTable != null && lastHitTable != table)
                 {
                     lastHitTable.SetHighlighted(false);
                 }
                 lastHitTable = table;
+
+                /* Interact */
+                ObjectInteract(table);
             }
             else if (lastHitTable != null)
             {
