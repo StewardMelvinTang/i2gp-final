754d16dcb156cf72a6ee99d43c67a99dfb1375dd
Skivap <aurickd@gmail.com>
2024-12-21 02:46:42 +0800

    Better Empty Crate

--- Assets/Script/Tables/FiniteCrate.cs
+++ Assets/Script/Tables/FiniteCrate.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,13 @@ using UnityEngine;
 public class FiniteCrate : Table
 {
     private StackFood stack;
+    private String name;
+
+    int idx = 0;
 
     protected void Start()
     {
+        idx = 0;
         tableRenderer = GetComponent<MeshRenderer>();
         if (tableRenderer != null)
         {
@@ -17,16 +22,42 @@ public class FiniteCrate : Table
         foodObject = new GameObject("Empty");
         stack = foodObject.AddComponent<StackFood>();
         foodObject.transform.SetParent(transform);
-        foodObject.transform.localPosition = new Vector3(0, 0, 0);
+        foodObject.transform.localPosition = new Vector3(0, -100, 0);
     }
 
     public override GameObject PutItem(GameObject gameObject){
+        if(gameObject == null) return null;
+        String curname = gameObject.GetComponent<Item>().itemName;
+        if(idx != 0) {
+            if (curname != name) {
+                return gameObject;
+            }
+        }
+        else {
+            name = curname;
+        }
+
         stack.InsertFood(gameObject);
+        idx++;
+        gameObject.transform.rotation = Quaternion.Euler(45f, 0f, 0f);
+        if(idx == 1) gameObject.transform.localPosition = new Vector3(0.345f, 100.2f, 0.4f);
+        else if(idx == 2) gameObject.transform.localPosition = new Vector3(0.345f, 100.2f, 0.1f);
+        else if(idx == 3) gameObject.transform.localPosition = new Vector3(0.345f, 100.2f, -0.2f);
+        else if(idx == 4) gameObject.transform.localPosition = new Vector3(0.345f, 100.2f, -0.5f);
+        else if(idx == 5) gameObject.transform.localPosition = new Vector3(-0.345f, 100.2f, 0.4f);
+        else if(idx == 6) gameObject.transform.localPosition = new Vector3(-0.345f, 100.2f, 0.1f);
+        else if(idx == 7) gameObject.transform.localPosition = new Vector3(-0.345f, 100.2f, -0.2f);
+        else if(idx == 8) gameObject.transform.localPosition = new Vector3(-0.345f, 100.2f, -0.5f);
         return null;
     }
     
     public override GameObject TakeItem(){
         GameObject returnItem = stack.Pop();
+        if (returnItem != null)
+        {
+            returnItem.transform.rotation = Quaternion.identity;
+            idx--;
+        }
         return returnItem;
     }
 }
