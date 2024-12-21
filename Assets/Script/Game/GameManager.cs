d4c690da1b95b1b9936e8c336e4de9178ed9b851
audrey chung <audreychung2352002@gmail.com>
2024-12-21 18:53:07 +0800

    Add Game Timer

--- Assets/Script/Game/GameManager.cs
+++ Assets/Script/Game/GameManager.cs
@@ -8,10 +8,37 @@ public class GameManager : MonoBehaviour
     public OrderUiManager orderUiManager;
     // Start is called before the first frame update
     public CustomerManager customerManager;
+
+    [SerializeField] 
+    private float currentGameTime;
+
+    // 5 minutes?
+    private float totalGameTime = 300f;  
+
+
     void Start()
     {
         // a little bit ugly here but basically everytime the customerManager spawns a customer, we get the recipe and show in UI 
+        currentGameTime = totalGameTime;
+
         customerManager.startSpawningCustomers(recipeManager);
+    }
+
+    void Update() {
+        if (currentGameTime > 0) {
+            currentGameTime -= Time.deltaTime;
+        } else {
+            Debug.Log("Game is finished");
+        }
+    }
+
+
+
+    public float GetCurrentTime() {
+        return currentGameTime;
+    }
+    public float GetTotalGameTime() {
+        return totalGameTime;
     }
 
     // void OnCustomerSpawned(Recipe recipe) {
